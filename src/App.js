a48a34cf059c5fa6db95edb0271e0d06430836ee
Klayver Ximenes <klayverx@gmail.com>
2021-03-13 21:13:50 -0300

    estilização formulário

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Component, Fragment } from 'react';
 import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Container, Typography } from "@material-ui/core"
+import 'fontsource-roboto';
 
-function App() {
-  return (
-    <h1>Fomulário de Cadastro</h1>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center" >Fomulário de cadastro</Typography>
+        <FormularioCadastro />
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,47 @@
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
+
+function FormularioCadastro() {
+  return (
+    <form action="">
+      <TextField
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+
+      <TextField
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promocoes" defaultChecked color="primary" />}
+      />
+
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" defaultChecked color="primary" />}
+      />
+
+      <Button variant="contained" color="primary" type="submit">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+
+export default FormularioCadastro;
