419395d931b1a4fb0bc6275bdfb5d943254301a2
Klayver <klayverx@gmail.com>
2021-03-17 11:56:39 -0300

    adicionando estados

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField } from "@material-ui/core";
 
 function DadosUsuario({ aoEnviar }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        aoEnviar();
+        aoEnviar({ email, senha });
       }}
     >
       <TextField
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
         id="email"
         label="Email"
         type="email"
@@ -18,8 +25,12 @@ function DadosUsuario({ aoEnviar }) {
         margin="normal"
         fullWidth
       />
-      
+
       <TextField
+        value={senha}
+        onChange={(e) => {
+          setSenha(e.target.value);
+        }}
         id="senha"
         label="Senha"
         type="password"
