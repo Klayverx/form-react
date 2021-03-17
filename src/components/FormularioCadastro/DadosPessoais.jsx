297a96a0cfb7cf92d2024441f00d38b7925e4ebc
Klayver <klayverx@gmail.com>
2021-03-17 15:03:21 -0300

    hook useErros

--- src/components/FormularioCadastro/DadosPessoais.jsx
+++ src/components/FormularioCadastro/DadosPessoais.jsx
@@ -1,5 +1,7 @@
 import { useState, useContext } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
+import useErros from "../../hooks/useErros";
+
 import ValidacoesCadastro from "../../contexts/validacoesCadastro";
 
 function DadosPessoais({ aoEnviar }) {
@@ -8,29 +10,10 @@ function DadosPessoais({ aoEnviar }) {
   const [cpf, setCpf] = useState("");
   const [promocoes, setPromocoes] = useState(true);
   const [novidades, setNovidades] = useState(true);
-  const [erros, setErros] = useState({
-    cpf: { valido: true, texto: "" },
-    nome: { valido: true, texto: "" },
-  });
 
   const validacoes = useContext(ValidacoesCadastro);
 
-  function validarCampos(e) {
-    const { name, value } = e.target;
-    const novoEstado = { ...erros };
-    novoEstado[name] = validacoes[name](value);
-
-    setErros(novoEstado);
-  }
-
-  function possoEnviar() {
-    for (let campo in erros) {
-      if (!erros[campo].valido) {
-        return false;
-      }
-    }
-    return true;
-  }
+  const [erros, validarCampos, possoEnviar] = useErros(validacoes);
 
   return (
     <form
--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,30 +1,14 @@
 import React, { useContext, useState } from "react";
 import { Button, TextField } from "@material-ui/core";
 import ValidacoesCadastro from "../../contexts/validacoesCadastro";
+import useErros from "../../hooks/useErros";
 
 function DadosUsuario({ aoEnviar }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
-  const [erros, setErros] = useState({ senha: { valido: true, texto: "" } });
-
   const validacoes = useContext(ValidacoesCadastro);
 
-  function validarCampos(e) {
-    const { name, value } = e.target;
-    const novoEstado = { ...erros };
-    novoEstado[name] = validacoes[name](value);
-
-    setErros(novoEstado);
-  }
-
-  function possoEnviar() {
-    for (let campo in erros) {
-      if (!erros[campo].valido) {
-        return false;
-      }
-    }
-    return true;
-  }
+  const [erros, validarCampos, possoEnviar] = useErros(validacoes);
 
   return (
     <form
