4077d7bebe38c780662da972d6a21a4ccf0a4063
Klayver <klayverx@gmail.com>
2021-03-17 10:45:16 -0300

    função de submit

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import { Button, TextField } from "@material-ui/core";
 
-function DadosUsuario() {
+function DadosUsuario({ aoEnviar }) {
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        aoEnviar();
+      }}
+    >
       <TextField
         id="email"
         label="Email"
         type="email"
+        required
         variant="outlined"
         margin="normal"
         fullWidth
       />
+      
       <TextField
         id="senha"
         label="Senha"
         type="password"
+        required
         variant="outlined"
         margin="normal"
         fullWidth
       />
+
       <Button variant="contained" color="primary" type="submit">
         Cadastrar
       </Button>
